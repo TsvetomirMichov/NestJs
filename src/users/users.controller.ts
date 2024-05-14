cedfef138ef2228feafa84ffdd5b8d450e526b6f
TsvetomirMichov <cvetimichov38@gmail.com>
2024-05-14 11:18:43 +0300

    Update services and controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor (private readonly usersService: UsersService){}
+
+    @Get()
+    findAll(@Query('role') role?: "ADMIN" | "ENGINEER" | "INTERN") {
+        return this.usersService.findAll(role);
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.usersService.findOne(+ id);
+    }
+
+    @Post()
+    create(@Body() user: {name: string, email: string, role: "ADMIN" | "ENGINEER" | "INTERN"}) {
+        return this.usersService.create(user)
+    }
+
+    @Patch(':id') // It can update one or many specific user fields
+    update(@Param('id') id: string, @Body() userUpdate: {name?: string, email?: string, role?: "ADMIN" | "ENGINEER" | "INTERN" }) {
+        return this.usersService.update(+id,userUpdate)
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.usersService.delete(+id)
+    }
+}
