de9bbfe7644f2c015e0692f0982890390cbfeca0
TsvetomirMichov <cvetimichov38@gmail.com>
2024-05-23 13:06:04 +0300

    Update-EmployeesServices

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -6,22 +6,42 @@ export class EmployeesService {
   constructor(private readonly databaseService: DatabaseService) { }
 
   async create(createEmployeeDto: Prisma.EmployeeCreateInput) {
-    return 'This action adds a new employee';
-  }
+    return this.databaseService.employee.create({
+      data:createEmployeeDto
+    })
+  } 
 
-  async findAll() {
-    return `This action returns all employees`;
+  async findAll(role?: "ADMIN" | "ENGINEER" | "INTERN") {
+   if(role) return this.databaseService.employee.findMany({
+      where:{
+        role,
+      }
+    })
+    return this.databaseService.employee.findMany()
   }
 
   async findOne(id: number) {
-    return `This action returns a #${id} employee`;
+    return this.databaseService.employee.findUnique({
+      where: {
+        id,
+      }
+    })
   }
 
   async update(id: number, updateEmployeeDto: Prisma.EmployeeUpdateInput) {
-    return `This action updates a #${id} employee`;
+    return this.databaseService.employee.update({
+      where:{
+        id,
+      },
+      data:updateEmployeeDto,
+    })
   }
 
   async remove(id: number) {
-    return `This action removes a #${id} employee`;
+    return this.databaseService.employee.delete({
+      where: {
+        id,
+      }
+    })
   }
 }
